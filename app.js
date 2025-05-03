be2e3075ca2cfdf512413260f4d28bae985caf19
Manoj Baskar <147849968+manojaba@users.noreply.github.com>
2025-05-03 06:48:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const validator = require('validator');
+const getNotes = require('./notes.js');
+
+console.log(getNotes());
+
+console.log(validator.isURL('https/www.npmjs.com/package/validator'))
+
+
+
+
+
+
+
+
+
+
+
+// const add = require('./name.js')
+
+
+// console.log(add(2, 3))
